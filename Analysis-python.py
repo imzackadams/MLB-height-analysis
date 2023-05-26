5b2c3d96c20bfac86f6c1357aa6750de1266f620
Zack Adams <imzackadams@protonmail.com>
2023-05-26 03:58:15 -0400

    add charts

--- Analysis-python.py
+++ Analysis-python.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[7]:
+# In[17]:
 
 
 import pandas as pd
 from pybaseball import batting_stats
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
@@ -15,7 +17,7 @@ pd.set_option('display.max_columns', None)
 all_years_data = pd.DataFrame()
 
 # Loop through the years of interest
-for year in range(2018, 2023):  # 2023 is the stop argument and it's exclusive
+for year in range(2015, 2023):  # 2023 is the stop argument and it's exclusive
     # Fetch batting stats for a given year
     year_data = batting_stats(year)
     
@@ -25,10 +27,10 @@ for year in range(2018, 2023):  # 2023 is the stop argument and it's exclusive
     # Append the yearly data to the overall DataFrame
     all_years_data = pd.concat([all_years_data, year_data])
 
-# Now 'all_years_data' holds data from 2018 to 2022
+# Now 'all_years_data' holds data from 2012 to 2022
 
 
-# In[8]:
+# In[18]:
 
 
 all_years_data.head()
@@ -40,7 +42,7 @@ all_years_data.head()
 
 
 
-# In[9]:
+# In[19]:
 
 
 # Load the People.csv file from Lahman's Baseball Database
@@ -53,13 +55,13 @@ height_df = people_df[['playerID', 'height']]
 print(height_df)
 
 
-# In[10]:
+# In[20]:
 
 
 people_df['Name'] = people_df['nameFirst'] + ' ' + people_df['nameLast']
 
 
-# In[11]:
+# In[21]:
 
 
 # Remove leading/trailing white space
@@ -71,44 +73,182 @@ people_df['Name'] = people_df['Name'].str.title()
 all_years_data['Name'] = all_years_data['Name'].str.title()
 
 
-# In[12]:
+# In[22]:
 
 
 height_df = people_df[['Name', 'height']]
 
 
-# In[15]:
+# In[23]:
 
 
-height_df.head()
+height_df["height"].describe()
 
 
-# In[16]:
+# In[24]:
 
 
 all_data_with_height = pd.merge(all_years_data, height_df, on='Name', how='inner')
 
 
-# In[18]:
+# In[25]:
 
 
-all_data_with_height
+all_data_with_height.tail()
 
 
-# In[22]:
+# In[26]:
 
 
 # Group# Group by 'Name' and 'height', calculate the mean, then reset index
-player_avg_data = all_data_with_height.groupby(['Name', 'height'])[['AVG', 'OBP', 'SLG']].mean().reset_index()
+player_avg_data = all_data_with_height.groupby(['Name', 'height'])[['AVG', 'OBP', 'SLG', 'BB', 'IBB', 'SB']].mean().reset_index()
 
-# Now 'player_avg_data' holds the average performance statistics for each player over the years 2018-2022, 
+# Now 'player_avg_data' holds the average performance statistics for each player over the years 2012-2022, 
 # along with each player's height
 
 
-# In[23]:
+# In[27]:
+
+
+player_avg_data.describe()
+
+
+# In[28]:
+
+
+# Calculate the average height
+average_height = height_df['height'].mean()
+
+# Display the average height
+print("Average height of players:", average_height)
+
+
+# In[29]:
+
+
+def height_category(height):
+    if height < 72:  # less than 72 inches
+        return 'Short'
+    elif height <= 75:  # 73 to 75 inches
+        return 'Average'
+    else:  # 76 inches and taller
+        return 'Tall'
+
+# Apply the function to create a new 'Height Category' column
+player_avg_data['Height Category'] = player_avg_data['height'].apply(height_category)
+
+
+# In[30]:
 
 
 player_avg_data
+
+
+# In[31]:
+
+
+# Set up the matplotlib figure
+f, axes = plt.subplots(6, 1, figsize=(7, 15), sharex=True)
+
+sns.barplot(x='Height Category', y='AVG', data=player_avg_data, ax=axes[0])
+axes[0].set_ylabel('Average Batting Average')
+
+sns.barplot(x='Height Category', y='OBP', data=player_avg_data, ax=axes[1])
+axes[1].set_ylabel('Average On-Base Percentage')
+
+sns.barplot(x='Height Category', y='SLG', data=player_avg_data, ax=axes[2])
+axes[2].set_ylabel('Average Slugging Percentage')
+
+sns.barplot(x='Height Category', y='BB', data=player_avg_data, ax=axes[3])
+axes[3].set_ylabel('Average Base on Balls')
+
+sns.barplot(x='Height Category', y='IBB', data=player_avg_data, ax=axes[4])
+axes[4].set_ylabel('Average Intentional Base on Balls')
+
+sns.barplot(x='Height Category', y='SB', data=player_avg_data, ax=axes[5])
+axes[5].set_ylabel('Average Stolen Bases')
+
+plt.tight_layout()
+
+
+# In[32]:
+
+
+f, axes = plt.subplots(6, 1, figsize=(7, 15), sharex=True)
+
+sns.boxplot(x='Height Category', y='AVG', data=player_avg_data, ax=axes[0])
+axes[0].set_ylabel('Batting Average')
+
+sns.boxplot(x='Height Category', y='OBP', data=player_avg_data, ax=axes[1])
+axes[1].set_ylabel('On-Base Percentage')
+
+sns.boxplot(x='Height Category', y='SLG', data=player_avg_data, ax=axes[2])
+axes[2].set_ylabel('Slugging Percentage')
+
+sns.boxplot(x='Height Category', y='BB', data=player_avg_data, ax=axes[3])
+axes[3].set_ylabel('Base on Balls')
+
+sns.boxplot(x='Height Category', y='IBB', data=player_avg_data, ax=axes[4])
+axes[4].set_ylabel('Intentional Base on Balls')
+
+sns.boxplot(x='Height Category', y='SB', data=player_avg_data, ax=axes[5])
+axes[5].set_ylabel('Stolen Bases')
+
+plt.tight_layout()
+
+
+
+# In[33]:
+
+
+f, axes = plt.subplots(6, 1, figsize=(7, 15), sharex=True)
+
+sns.scatterplot(x='height', y='AVG', hue='Height Category', data=player_avg_data, ax=axes[0])
+axes[0].set_ylabel('Batting Average')
+
+sns.scatterplot(x='height', y='OBP', hue='Height Category', data=player_avg_data, ax=axes[1])
+axes[1].set_ylabel('On-Base Percentage')
+
+sns.scatterplot(x='height', y='SLG', hue='Height Category', data=player_avg_data, ax=axes[2])
+axes[2].set_ylabel('Slugging Percentage')
+
+sns.scatterplot(x='height', y='BB', hue='Height Category', data=player_avg_data, ax=axes[3])
+axes[3].set_ylabel('Base on Balls')
+
+sns.scatterplot(x='height', y='IBB', hue='Height Category', data=player_avg_data, ax=axes[4])
+axes[4].set_ylabel('Intentional Base on Balls')
+
+sns.scatterplot(x='height', y='SB', hue='Height Category', data=player_avg_data, ax=axes[5])
+axes[5].set_ylabel('Stolen Bases')
+
+plt.tight_layout()
+
+
+# In[34]:
+
+
+sns.pairplot(player_avg_data[['AVG', 'OBP', 'SLG', 'BB', 'IBB', 'SB', 'height']])
+
+
+# In[40]:
+
+
+# Scatter plot of AVG vs. OBP with green markers
+sns.scatterplot(x='AVG', y='OBP', hue='height', data=player_avg_data, color='green')
+plt.title('AVG vs. OBP')
+plt.show()
+
+# Scatter plot of SLG vs. BB with green markers
+sns.scatterplot(x='SLG', y='BB', hue='height', data=player_avg_data, color='green')
+plt.title('SLG vs. BB')
+plt.show()
+
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
